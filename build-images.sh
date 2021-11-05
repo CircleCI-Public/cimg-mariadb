4e850f67515e9ad0677468f9ebc42e265ad8e036
Adam Hartley <BytesGuy@users.noreply.github.com>
2021-11-05 10:26:03 +0000

    Remove 10.2 as it does not have a package for Focal

--- build-images.sh
+++ build-images.sh
@@ -4,4 +4,3 @@ docker build --file 10.6/Dockerfile -t cimg/mariadb:10.6.4  -t cimg/mariadb:10.6
 docker build --file 10.5/Dockerfile -t cimg/mariadb:10.5.12  -t cimg/mariadb:10.5 .
 docker build --file 10.4/Dockerfile -t cimg/mariadb:10.4.21  -t cimg/mariadb:10.4 .
 docker build --file 10.3/Dockerfile -t cimg/mariadb:10.3.31  -t cimg/mariadb:10.3 .
-docker build --file 10.2/Dockerfile -t cimg/mariadb:10.2.40  -t cimg/mariadb:10.2 .
