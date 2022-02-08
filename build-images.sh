74088e1785664d2493d90d9a90c5ae8907ed974c
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-02-08 23:05:59 +0000

    Pub: v10.6.6, v10.5.14, v10.4.23, and more. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 10.3/Dockerfile -t cimg/mariadb:10.3.32  -t cimg/mariadb:10.3 .
-docker build --file 10.4/Dockerfile -t cimg/mariadb:10.4.22  -t cimg/mariadb:10.4 .
-docker build --file 10.5/Dockerfile -t cimg/mariadb:10.5.13  -t cimg/mariadb:10.5 .
-docker build --file 10.6/Dockerfile -t cimg/mariadb:10.6.5  -t cimg/mariadb:10.6 .
+docker build --file 10.6/Dockerfile -t cimg/mariadb:10.6.6  -t cimg/mariadb:10.6 .
+docker build --file 10.5/Dockerfile -t cimg/mariadb:10.5.14  -t cimg/mariadb:10.5 .
+docker build --file 10.4/Dockerfile -t cimg/mariadb:10.4.23  -t cimg/mariadb:10.4 .
+docker build --file 10.3/Dockerfile -t cimg/mariadb:10.3.33  -t cimg/mariadb:10.3 .
---
+++ push-images.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker push cimg/mariadb:10.6.6
+docker push cimg/mariadb:10.6
+
+docker push cimg/mariadb:10.5.14
+docker push cimg/mariadb:10.5
+
+docker push cimg/mariadb:10.4.23
+docker push cimg/mariadb:10.4
+
+docker push cimg/mariadb:10.3.33
+docker push cimg/mariadb:10.3
