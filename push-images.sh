aef42e5ad9f3bd623e28ef44d8491352d12c6afc
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-02-20 15:38:46 +0000

    Pub: v10.6.7, v10.5.15, v10.4.24, and more. [release]

---
+++ push-images.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker push cimg/mariadb:10.6.7
+docker push cimg/mariadb:10.6
+
+docker push cimg/mariadb:10.5.15
+docker push cimg/mariadb:10.5
+
+docker push cimg/mariadb:10.4.24
+docker push cimg/mariadb:10.4
+
+docker push cimg/mariadb:10.3.34
+docker push cimg/mariadb:10.3
