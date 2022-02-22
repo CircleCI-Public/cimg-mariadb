b7f1df6492aba2053f7646f361df754a4f67ca01
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-02-22 05:34:24 +0000

    Pub: v10.6.7, v10.5.15, v10.4.24, and more. [release] (#19)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 10.3/Dockerfile -t cimg/mariadb:10.3.32  -t cimg/mariadb:10.3 .
-docker build --file 10.4/Dockerfile -t cimg/mariadb:10.4.22  -t cimg/mariadb:10.4 .
-docker build --file 10.5/Dockerfile -t cimg/mariadb:10.5.13  -t cimg/mariadb:10.5 .
-docker build --file 10.6/Dockerfile -t cimg/mariadb:10.6.5  -t cimg/mariadb:10.6 .
+docker build --file 10.6/Dockerfile -t cimg/mariadb:10.6.7  -t cimg/mariadb:10.6 .
+docker build --file 10.5/Dockerfile -t cimg/mariadb:10.5.15  -t cimg/mariadb:10.5 .
+docker build --file 10.4/Dockerfile -t cimg/mariadb:10.4.24  -t cimg/mariadb:10.4 .
+docker build --file 10.3/Dockerfile -t cimg/mariadb:10.3.34  -t cimg/mariadb:10.3 .
