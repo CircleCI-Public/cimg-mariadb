b3f3f369bd4e4421429f9132800ba51557f819b6
Jeff Chen <jeff.chen@circleci.com>
2022-05-26 11:36:56 -0700

    Publish v10.3.35. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 10.3/Dockerfile -t cimg/mariadb:10.3.35  -t cimg/mariadb:10.3 .
+docker push cimg/mariadb:10.3.35
+docker push cimg/mariadb:10.3
