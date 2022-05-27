b8d4a9e0c33e42231fc5d34892da37c7ebc7c4f3
Jalexchen <jeff.chen@circleci.com>
2022-05-27 10:38:44 -0700

    Publish v10.3.35. [release] (#21)

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 10.5/Dockerfile -t cimg/mariadb:10.5.16  -t cimg/mariadb:10.5 .
-docker build --file 10.4/Dockerfile -t cimg/mariadb:10.4.25  -t cimg/mariadb:10.4 .
-docker build --file 10.6/Dockerfile -t cimg/mariadb:10.6.8  -t cimg/mariadb:10.6 .
-docker build --file 10.7/Dockerfile -t cimg/mariadb:10.7.4  -t cimg/mariadb:10.7 .
-docker build --file 10.8/Dockerfile -t cimg/mariadb:10.8.3  -t cimg/mariadb:10.8 .
+docker build --file 10.3/Dockerfile -t cimg/mariadb:10.3.35  -t cimg/mariadb:10.3 .
--- push-images.sh
+++ push-images.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/mariadb:10.5.16
-docker push cimg/mariadb:10.5
-
-docker push cimg/mariadb:10.4.25
-docker push cimg/mariadb:10.4
-
-docker push cimg/mariadb:10.6.8
-docker push cimg/mariadb:10.6
-
-docker push cimg/mariadb:10.7.4
-docker push cimg/mariadb:10.7
-
-docker push cimg/mariadb:10.8.3
-docker push cimg/mariadb:10.8
+docker push cimg/mariadb:10.3.35
+docker push cimg/mariadb:10.3
