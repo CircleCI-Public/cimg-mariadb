db420b742911fa7dd25ae9364858f2a2674cb216
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2022-08-15 14:22:05 -0400

    Pub: 10.10.1,10.9.2,10.8.4,10.7.5, and more. [release] (#23)

--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/mariadb:10.3.35
+docker push cimg/mariadb:10.10.1
+docker push cimg/mariadb:10.10
+
+docker push cimg/mariadb:10.9.2
+docker push cimg/mariadb:10.9
+
+docker push cimg/mariadb:10.8.4
+docker push cimg/mariadb:10.8
+
+docker push cimg/mariadb:10.7.5
+docker push cimg/mariadb:10.7
+
+docker push cimg/mariadb:10.6.9
+docker push cimg/mariadb:10.6
+
+docker push cimg/mariadb:10.5.17
+docker push cimg/mariadb:10.5
+
+docker push cimg/mariadb:10.4.26
+docker push cimg/mariadb:10.4
+
+docker push cimg/mariadb:10.3.36
 docker push cimg/mariadb:10.3
