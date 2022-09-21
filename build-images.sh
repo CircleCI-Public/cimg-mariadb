b9ba439dc50954a3b353bc9a3f79ff29b2c41b0c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-09-21 12:08:38 -0400

    Pub: v10.6.10, v10.7.6, v10.8.5, and more. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 10.10/Dockerfile -t cimg/mariadb:10.10.1  -t cimg/mariadb:10.10 .
-docker build --file 10.9/Dockerfile -t cimg/mariadb:10.9.2  -t cimg/mariadb:10.9 .
-docker build --file 10.8/Dockerfile -t cimg/mariadb:10.8.4  -t cimg/mariadb:10.8 .
-docker build --file 10.7/Dockerfile -t cimg/mariadb:10.7.5  -t cimg/mariadb:10.7 .
-docker build --file 10.6/Dockerfile -t cimg/mariadb:10.6.9  -t cimg/mariadb:10.6 .
-docker build --file 10.5/Dockerfile -t cimg/mariadb:10.5.17  -t cimg/mariadb:10.5 .
-docker build --file 10.4/Dockerfile -t cimg/mariadb:10.4.26  -t cimg/mariadb:10.4 .
-docker build --file 10.3/Dockerfile -t cimg/mariadb:10.3.36  -t cimg/mariadb:10.3 .
+docker build --file 10.6/Dockerfile -t cimg/mariadb:10.6.10  -t cimg/mariadb:10.6 .
+docker build --file 10.7/Dockerfile -t cimg/mariadb:10.7.6  -t cimg/mariadb:10.7 .
+docker build --file 10.8/Dockerfile -t cimg/mariadb:10.8.5  -t cimg/mariadb:10.8 .
+docker build --file 10.9/Dockerfile -t cimg/mariadb:10.9.3  -t cimg/mariadb:10.9 .
--- push-images.sh
+++ push-images.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/mariadb:10.10.1
-docker push cimg/mariadb:10.10
-
-docker push cimg/mariadb:10.9.2
-docker push cimg/mariadb:10.9
-
-docker push cimg/mariadb:10.8.4
-docker push cimg/mariadb:10.8
-
-docker push cimg/mariadb:10.7.5
-docker push cimg/mariadb:10.7
-
-docker push cimg/mariadb:10.6.9
+docker push cimg/mariadb:10.6.10
 docker push cimg/mariadb:10.6
 
-docker push cimg/mariadb:10.5.17
-docker push cimg/mariadb:10.5
+docker push cimg/mariadb:10.7.6
+docker push cimg/mariadb:10.7
 
-docker push cimg/mariadb:10.4.26
-docker push cimg/mariadb:10.4
+docker push cimg/mariadb:10.8.5
+docker push cimg/mariadb:10.8
 
-docker push cimg/mariadb:10.3.36
-docker push cimg/mariadb:10.3
+docker push cimg/mariadb:10.9.3
+docker push cimg/mariadb:10.9
