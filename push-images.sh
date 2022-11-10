9318165da694421e56c1b3233748d154a0ead701
Jalexchen <jeff.chen@circleci.com>
2022-11-10 10:12:17 -0800

    Pub: 10.3.37,10.4.27,10.5.18,10.6.11, and more. [release] (#26)

--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/mariadb:10.6.10
+docker push cimg/mariadb:10.3
+docker push cimg/mariadb:10.3.37
+docker push cimg/mariadb:10.4
+docker push cimg/mariadb:10.4.27
+docker push cimg/mariadb:10.5
+docker push cimg/mariadb:10.5.18
 docker push cimg/mariadb:10.6
-
-docker push cimg/mariadb:10.7.6
+docker push cimg/mariadb:10.6.11
 docker push cimg/mariadb:10.7
-
-docker push cimg/mariadb:10.8.5
+docker push cimg/mariadb:10.7.7
 docker push cimg/mariadb:10.8
-
-docker push cimg/mariadb:10.9.3
+docker push cimg/mariadb:10.8.6
 docker push cimg/mariadb:10.9
+docker push cimg/mariadb:10.9.4
+docker push cimg/mariadb:10.10
+docker push cimg/mariadb:10.10.2
