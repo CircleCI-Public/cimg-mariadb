15bddbe17c85fec9c754c6801053fe9d16110219
Adam Hartley <git@ahartley.com>
2023-07-20 13:06:03 +0100

    Test without 10.4

--- build-images.sh
+++ build-images.sh
@@ -10,4 +10,3 @@ docker buildx build --platform=linux/amd64,linux/arm64 --file 10.10/Dockerfile -
 docker buildx build --platform=linux/amd64,linux/arm64 --file 10.9/Dockerfile -t cimg/mariadb:10.9.7 -t cimg/mariadb:10.9 --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file 10.6/Dockerfile -t cimg/mariadb:10.6.14 -t cimg/mariadb:10.6 --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file 10.5/Dockerfile -t cimg/mariadb:10.5.21 -t cimg/mariadb:10.5 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 10.4/Dockerfile -t cimg/mariadb:10.4.30 -t cimg/mariadb:10.4 --push .
