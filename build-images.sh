49b291ef699304ea05b3fdcd9708c1d1f559e1e9
Adam Hartley <git@ahartley.com>
2023-07-20 10:24:16 +0100

    Test build 11.0.2

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 10.11/Dockerfile -t cimg/mariadb:10.11.2 -t cimg/mariadb:10.11 --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 11.0/Dockerfile -t cimg/mariadb:11.0.2 -t cimg/mariadb:11.0 --push .
