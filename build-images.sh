7c83e27611d2e770c1f5cc58a60fe190d09f64df
Adam Hartley <git@ahartley.com>
2023-07-20 20:22:55 +0100

    Pub: 11.0.2,10.11.4,10.10.5,10.9.7, and more. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 10.11/Dockerfile -t cimg/mariadb:10.11.2 -t cimg/mariadb:10.11 --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 11.0/Dockerfile -t cimg/mariadb:11.0.2 -t cimg/mariadb:11.0 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 10.11/Dockerfile -t cimg/mariadb:10.11.4 -t cimg/mariadb:10.11 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 10.10/Dockerfile -t cimg/mariadb:10.10.5 -t cimg/mariadb:10.10 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 10.9/Dockerfile -t cimg/mariadb:10.9.7 -t cimg/mariadb:10.9 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 10.6/Dockerfile -t cimg/mariadb:10.6.14 -t cimg/mariadb:10.6 --push .
--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/mariadb:10.11
-docker push cimg/mariadb:10.11.2
+set -eo pipefail
