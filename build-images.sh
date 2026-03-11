5ff332171be26d6f4f301eb2e2592f0b3de8821e
Anthony Castro <anthony.castro@circleci.com>
2026-03-11 13:31:53 -0400

    Add 12.3.1

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,4 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 11.7/Dockerfile -t cimg/mariadb:11.7.1 -t cimg/mariadb:11.7 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 11.6/Dockerfile -t cimg/mariadb:11.6.2 -t cimg/mariadb:11.6 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 11.4/Dockerfile -t cimg/mariadb:11.4.4 -t cimg/mariadb:11.4 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 12.3/Dockerfile -t cimg/mariadb:12.3.1 -t cimg/mariadb:12.3 --push .
