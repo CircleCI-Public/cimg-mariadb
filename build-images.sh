e64d41e304482e4c5a43b463176166505746acb8
Anthony Castro <anthony-j-castro@users.noreply.github.com>
2026-03-17 12:59:13 -0400

    Pub: 11.8.6,12.0.2,12.1.2,12.2.2 [release] (#63)

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 11.7/Dockerfile -t cimg/mariadb:11.7.1 -t cimg/mariadb:11.7 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 11.6/Dockerfile -t cimg/mariadb:11.6.2 -t cimg/mariadb:11.6 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 11.4/Dockerfile -t cimg/mariadb:11.4.4 -t cimg/mariadb:11.4 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 11.8/Dockerfile -t cimg/mariadb:11.8.6 -t cimg/mariadb:11.8 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 12.0/Dockerfile -t cimg/mariadb:12.0.2 -t cimg/mariadb:12.0 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 12.1/Dockerfile -t cimg/mariadb:12.1.2 -t cimg/mariadb:12.1 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 12.2/Dockerfile -t cimg/mariadb:12.2.2 -t cimg/mariadb:12.2 --push .
